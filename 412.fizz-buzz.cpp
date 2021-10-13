ebde6dfc25e1c0c4b3beba4e0a48943dd6997499
zydiii <15826138027@163.com>
2021-10-13 09:52:23 +0800

    412

---
+++ 412.fizz-buzz.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=412 lang=cpp
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+
+        for(int i = 1; i <= n; i++)
+        {
+            if(i % 15 == 0)
+            {
+                result.push_back("FizzBuzz");
+            }
+            else if(i % 3 == 0)
+            {
+                result.push_back("Fizz");
+            }
+            else if(i % 5 == 0)
+            {
+                result.push_back("Buzz");
+            }
+            else
+            {
+                result.push_back(to_string(i));
+            }
+        }      
+
+        return result;  
+    }
+};
+// @lc code=end
+
