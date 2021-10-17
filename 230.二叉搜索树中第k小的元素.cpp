d974859b4c751493dec09b8a546b0f2345127306
zydiii <zydiiii@outlook.com>
2021-10-17 10:08:10 +0800

    230

---
+++ 230.二叉搜索树中第k小的元素.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=cpp
+ *
+ * [230] 二叉搜索树中第K小的元素
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> s;
+
+        while(root != nullptr || s.size() > 0)
+        {
+            while(root != nullptr)
+            {
+                s.push(root);
+                root = root->left;
+            }
+
+            root = s.top();
+            s.pop();
+            
+            k--;
+            if(k == 0)
+                break;
+            
+            root = root->right;
+        }
+
+        return root->val;
+    }
+};
+// @lc code=end
+
