2013ae7462dd0dae26f3abd5b983736537c7dcab
zydiii <zydiiii@outlook.com>
2022-02-18 10:08:15 +0800

    1791

--- 1791. 找出星型图的中心节点.cpp
+++ 1791. 找出星型图的中心节点.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        map<int, vector<int>> edgeMap;
-        for(vector<int> edge : edges)
-        {
-            edgeMap[edge[0]].push_back(edge[1]);
-            edgeMap[edge[1]].push_back(edge[0]);
-        }
-        map<int, vector<int>> :: iterator it;
-        for(it = edgeMap.begin(); it != edgeMap.end(); it++)
-        {
-            if(it->second.size() == edges.size())
-                return it->first;
-        }
-
-        return -1;
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
     }
 };
