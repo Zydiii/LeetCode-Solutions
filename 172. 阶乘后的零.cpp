6f39a60a75331e919838d98ffcec945405d9d37d
zydiii <zydiiii@outlook.com>
2022-03-25 22:24:11 +0800

    172

---
+++ 172. 阶乘后的零.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        for(int i = 5; i <= n; i+=5)
+        {
+            for (int x = i; x % 5 == 0; x /= 5) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
