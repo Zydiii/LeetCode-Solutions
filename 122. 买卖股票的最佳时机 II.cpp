99c37fd8bd062d7462677f095a32120c5c9d06f9
zydiii <zydiiii@outlook.com>
2022-04-15 20:22:52 +0800

    122

---
+++ 122. 买卖股票的最佳时机 II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int dp0 = 0, dp1 = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            int newDp0 = max(dp0, dp1 + prices[i]);
+            int newDp1 = max(dp1, dp0 - prices[i]);
+            dp0 = newDp0;
+            dp1 = newDp1;
+        }
+        return dp0;
+    }
+};
