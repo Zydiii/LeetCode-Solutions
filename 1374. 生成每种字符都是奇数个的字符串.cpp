46ead9af650e70db468b4a491864af98da3f0530
zydiii <zydiiii@outlook.com>
2022-08-01 16:42:17 +0800

    1374

---
+++ 1374. 生成每种字符都是奇数个的字符串.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        if(n % 2){
+            return string(n, 's');
+        }
+        else{
+            return string(n - 1, 's') + "x";
+        }
+    }
+};
